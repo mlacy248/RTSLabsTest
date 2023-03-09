a51435d8d631a718eef01687852e290a6fd70195
MichaelaLacy <mlacy248@gmail.com>
2023-03-08 20:24:18 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Console.WriteLine("Hello, World!");
+
+namespace Code
+{
+    public class mainClass{
+        static void Main()
+        {
+            //aboveBelow test setup- simple input
+
+            Console.WriteLine("Above Below Tests...");
+
+            int[] testList1 = new int[]
+            {
+                1,5,4,9,11,3,6,12,20,2
+            };
+
+            int testNumber1 = 8;
+
+            Dictionary<string, int> aboveBelowDictionary1 = RtsCodeTest.aboveBelow(testList1, testNumber1);
+            
+            Console.WriteLine("Standard Input Test:");
+            Console.WriteLine("above:  " + aboveBelowDictionary1["above"]);
+            Console.WriteLine("below:  " + aboveBelowDictionary1["below"]);
+            
+            //aboveBelow test setup- null list, should return 0 for both above and below
+            int[] testList2 = null;
+
+            Dictionary<string, int> aboveBelowDictionary2 = RtsCodeTest.aboveBelow(testList2, testNumber1);
+
+            Console.WriteLine("Null List Test:");
+            Console.WriteLine("above:  " + aboveBelowDictionary2["above"]);
+            Console.WriteLine("below:  " + aboveBelowDictionary2["below"]);
+
+            //aboveBelow test setup- integer equal to one value in list, should return a total one less than length
+
+            int testNumber2 = 9;
+
+            Dictionary<string, int> aboveBelowDictionary3 = RtsCodeTest.aboveBelow(testList1, testNumber2);
+            
+            Console.WriteLine("List Contains Equal Value Test:");
+            Console.WriteLine("above:  " + aboveBelowDictionary3["above"]);
+            Console.WriteLine("below:  " + aboveBelowDictionary3["below"]);
+            Console.WriteLine("List Length:  " + testList1.Length + 
+                " not equal to total aboveBelow count: " + (aboveBelowDictionary3["above"] 
+                + aboveBelowDictionary3["below"]));
+
+            //stringRotation test setup, standard input
+
+            Console.WriteLine("String Rotation Tests...");
+
+            string testString1 = "Supercalifragilisticexpialidocious";
+            int testRotationAmount1 = 8;
+
+            Console.WriteLine("Standard Input Test");
+            Console.WriteLine(RtsCodeTest.stringRotation(testString1, testRotationAmount1));
+
+            string testString2 = null;
+
+            Console.WriteLine("Null String Input Test");
+            Console.WriteLine(RtsCodeTest.stringRotation(testString2, testRotationAmount1));
+
+            int testRotationAmount2 = 108;
+
+            Console.WriteLine("Rotation Amount Greater Than Length Input Test");
+            Console.WriteLine("string length: " + testString1.Length + ", rotation amount: " 
+                + testRotationAmount2);
+            Console.WriteLine(RtsCodeTest.stringRotation(testString1, testRotationAmount2));
+
+        }
+    }
+}
---
+++ RtsCodeTestClass.cs
@@ -0,0 +1,58 @@
+namespace Code
+{
+    public static class RtsCodeTest{
+        //constructor
+
+        //deconstructor
+
+        //aboveBelow
+        public static Dictionary<string, int> aboveBelow(int[] theList, int theComparisonValue)
+        {
+            Dictionary<string, int> aboveBelowRecord = new Dictionary<string, int>()
+            {
+                {"above", 0},
+                {"below", 0}
+            };
+
+            if(theList != null)
+            {
+                for(int i = 0; i < theList.Length; i++)
+                {
+                    if(theList[i] > theComparisonValue)
+                    {
+                        int aboveCount = aboveBelowRecord["above"] + 1;
+                        aboveBelowRecord["above"] = aboveCount;
+                    }
+                    else if(theList[i] < theComparisonValue)
+                    {
+                        int belowCount = aboveBelowRecord["below"] + 1;
+                        aboveBelowRecord["below"] = belowCount;
+                    }
+                }
+            }
+
+            return aboveBelowRecord;
+        }
+
+        //stringRotation
+
+        public static string stringRotation(string originalString, int rotationAmount){
+
+
+            if(originalString == null){
+                return "Please enter a non null string value";
+            }
+
+            if(originalString.Length < rotationAmount){
+                rotationAmount = rotationAmount%originalString.Length;
+            }
+
+            string lowerString = originalString.Substring(0, rotationAmount);
+            string upperString = originalString.Substring(rotationAmount, originalString.Length-rotationAmount);
+
+            string rotatedString = upperString + lowerString;
+
+            return rotatedString;
+        }
+    }
+}
